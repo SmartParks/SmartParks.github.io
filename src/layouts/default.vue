b9f2a3d97f0212caba4bf15997d53f867238f124
Loren <24988869+GeorgiaData@users.noreply.github.com>
2018-06-12 16:37:19 -0400

    Title

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -17,7 +17,7 @@
         </q-btn>
 
         <q-toolbar-title>
-          Data Portal
+          Quasar1
           <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
         </q-toolbar-title>
       </q-toolbar>
